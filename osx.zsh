eaa6a37320451b8aa602503ca55bccff2768e172
Abe Walters <Abe.Walters@SGCSNYMACAWALT.local>
2014-05-28 15:12:14 -0400

    Commented out and updated subl alias.

--- osx.zsh
+++ osx.zsh
@@ -4,7 +4,7 @@ if [ ! $(command -v sw_vers) ]; then return; fi
 alias batman="node -e 'console.log(Array(16).join(\"wat\" - 1).toLowerCase() + \"Batman!\")' | xargs say -v alex"
 alias chrome='open -a Google\ Chrome --args --disable-web-security'
 alias lestweforget="yes 'dawn' | xargs say -v 'Bad News'"
-alias subl='~/subl'
+# alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 
 # android
 export PATH="$PATH:/Users/awalters/Development/Android/sdk/platform-tools"
